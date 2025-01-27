2ebfc39ba70895ed91f68fb2d14805428a88be17
LiquidPeanuts <jchang9360@gmail.com>
2025-01-26 20:38:34 -0500

    add pathcheck and userinput

---
+++ userinputCLI.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Please specify file path" << endl;
+    string filePath;
+    cin >> filePath;
+    cout << "You entered: " << filePath << endl;
+
+
+    return 0;
+}
