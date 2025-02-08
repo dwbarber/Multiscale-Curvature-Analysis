e72c6371f6bf715c414a95ea0cf616cf7d0b6337
CSaeedHP <146278386+CSaeedHP@users.noreply.github.com>
2025-02-07 20:37:01 -0500

    File reading and file writing were merged into fileHandler.cpp. The fileWrite.cpp was deleted. The PCSVsin.csv file was added to the repository. The file reading will be pending the checking functions next push should include that

--- fileHandler.cpp
+++ fileHandler.cpp
@@ -1,88 +1,101 @@
-#include "fileHandler.h"
 #include "dataContainer.h"
 #include <fstream>
 #include <string>
-
 #include <iostream>
 #include <sstream>
-
 using namespace std;
 
-int main() {
-    std::ifstream file("PCSVsin.csv"); // Open file
-    
-    if (!file.is_open()) {
-        std::cerr << "Error opening file!" << std::endl;
-        return 1;
-    }
+class FileHandler {
+    public:
 
-    //std::vector<std::vector<double>> data; // Store the CSV values
-    std::string line;
-    int ArraySize = 0;
-    int ite = 0;
-
-    while (std::getline(file, line)) { // Read line by line
-        ArraySize++;
-    }
-
-    point* pointArray = new point[ArraySize];
-
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string x_str, z_str;
+// Function is meant to read a CSV file and return an array of points
+    point* fileRead() {
+        std::ifstream file("PCSVsin.csv"); // Open file
         
-        // Read the two values separated by a comma
-        if (std::getline(ss, x_str, ',') && std::getline(ss, z_str, ',')) {
-            pointArray[ite].x = std::stod(x_str);
-            pointArray[ite].z = std::stod(z_str);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file!" << std::endl;
+            point* error = new point[1];
+            error[0].x = -1;
+            error[0].z = -1;
+            return error;
+        }
+        else{
+            // First loop: Count the number of lines
+            std::string line;
+            int ArraySize = 0;
+            
+            while (std::getline(file, line)) {
+                ArraySize++;
+            }
+
+            // Reset file pointer
+            file.clear();               // Clear any error flags
+            file.seekg(0, ios::beg);    // Move pointer to the beginning of the file
+
+            // Allocate dynamic array
+            point* pointArray = new point[ArraySize];
+
+            // Second loop: Read data into array
+            int ite = 0;
+            while (std::getline(file, line)) {
+                std::stringstream ss(line);
+                std::string x_str, z_str;
+                
+                // Read the two values separated by a comma
+                if (std::getline(ss, x_str, ',') && std::getline(ss, z_str, ',')) {
+                    pointArray[ite].x = std::stod(x_str);
+                    pointArray[ite].z = std::stod(z_str);
+                    ite++;  // Increment index
+                }
+            }
+            file.close(); // Close file
+
+            std::cout << "Data was read successfully.\n";
+
+            // for testing purposes
+            // // Print the extracted data
+            // for (int i = 0; i < ArraySize; i++) {
+            //     std::cout << "X: " << pointArray[i].x << ", Z: " << pointArray[i].z << std::endl;
+            // }
+
+            return pointArray;
         }
     }
 
-    file.close(); // Close file
-
-    // // Print the extracted data
-    // for (const auto& row : data) {
-    //     for (double val : row) {
-    //         std::cout << val << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    return 0;
-}
-
-// point* FileHandler::read(string fileName){
-//     ifstream input;
-//     if(!input.open(fileName)){
-//         //throw error maybe?
-//     }
-//     double xval,zval;
-//     vector<point> import;
-//     point a;
-//     double doubletoignore;//there is unnecessary doubles that are just zero
-//     int nameOrorderofpoints;//there are int val inputs to ignore in each line
-//     string lastLineBefInput;
-//     while(!input.eof()&&(lastLineBefInput.std::find("// X,Y,Z,C,Lock")!=string::npos)){//"// X,Y,Z,C,Lock" is the last line before data
-//         getline(input, lastLineBefInput);
-//     }
-//     while(!input.eof()){
-//         input >> nameOrorderofpoints;
-//         input >> xval;
-//         input >> doubletoignore;
-//         input >> zval;
-//         input >> doubletoignore;
-//         input >> nameOrorderofpoints;
-//         a.x=xval;
-//         a.z=zval;
-//         import.push_back(a);
-//     }
-//     input.close();
-//     point* array=new point[import.size()];//make so array doesnt get deleted after function ends
-//     unsigned long long int a= import.size()// add long(s) if number of points possible needs to be bigger
-//     unsigned long long int i=0;
-//     while(i < a){
-//         array[i]= import.at(i);
-//         ++i;
-//     }
-//     return array;
-// }
+// Function is meant to write data to a CSV file
+    int fileWrite(DataContainer& XSC){
+        int ScaleSwitching = 0;
+        int CurvatureSwitching = 0;
+        std::string fileName;
+    
+        std::cout << "Enter file Name: ";
+        std::cin >> fileName;
+        fileName += ".csv";
+        std::ofstream myFile(fileName); // Creates or overwrites example.txt
+    
+        if (myFile.is_open()) {
+            int BroadArraySize = XSC.getCurvatureArrayLength(); // Length of the Array holding ALL of the Curvature Data
+            int CurvatureArraySize; // Length of the Array holding the Curvature Data for a Specific Scale
+            myFile << "X-Position" << "," << "Scale" << "," << "Curvature" << std::endl;
+            while (ScaleSwitching < BroadArraySize) {
+                CurvatureArraySize = XSC.getIndex(ScaleSwitching)->getLength();
+                while (CurvatureSwitching < CurvatureArraySize){
+                    myFile << XSC.getIndex(ScaleSwitching)->getPoint(CurvatureSwitching).x << "," 
+                           << XSC.getIndex(ScaleSwitching)->getScale() << "," 
+                           << XSC.getIndex(ScaleSwitching)->getCurvature(CurvatureSwitching) << std::endl;
+                    CurvatureSwitching++; // Changing the index for Array of points
+                }
+                ScaleSwitching++; // Changing the index for Array of Curvature Data
+            }
+    
+            myFile.close();
+            std::cout << "Data written to file successfully." << std::endl;
+        } 
+        else {
+            std::cerr << "Unable to open file." << std::endl;
+            return 1; // Indicate an error
+        }
+    
+        return 0;
+    }
+};
