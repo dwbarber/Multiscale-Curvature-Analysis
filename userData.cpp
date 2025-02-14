a1150db7410d2c19be278e3d3f2068493db35318
CSaeedHP <146278386+CSaeedHP@users.noreply.github.com>
2025-02-13 20:21:19 -0500

    Updated userData

--- userData.cpp
+++ userData.cpp
@@ -1,28 +1,50 @@
 #include "userData.h"
 #include "dataContainer.h"
+#include "userInputCLI.h"
+#include "fileHandler.h"
 #include <string>
 using namespace std;
 
     //setters
+
+    bool UserData::setDataContainer(DataContainer dataContainer){
+        // Assuming dataContainer is a DataContainer object
+        // You might need to implement a mapping from string to DataContainer
+        UserData::dataContainer = dataContainer;
+        return true;
+    }
     int UserData::setScaleBounds(double min, double max){
         //returns 0 if valid, 1 if minscale is invalid, 2 if maxscale is invalid, 3 if both are invalid
-        
-        
-        userMinScale = min;
-        userMaxScale = max;
-        if(( <= min) && (min <= max)){
+
+        int minint = floor(min / getDataContainer().getMinLength());
+        int maxint = ceil(max / getDataContainer().getMinLength());
+        bool minValid = true;
+        bool maxValid = true;
+
+        if(minint < 1){
+            minValid = false;
+        }
+        if(maxint > getDataContainer().getmaxHalfIntervalPossible()){
+            maxValid = false;
+        }
+
+        if(minValid && maxValid){
+            userMinScale = minint;
+            userMaxScale = maxint;
             return 0;
         }
-        else if()
-
-        int DataContainer::getCurvatureArrayLength() {
-            return curvatureArrayLength;
+        else if(!minValid && !maxValid){
+            //cerr << "Both min and max scale are invalid" << endl;
+            return 3;
         }
-
-        CurvatureScale* DataContainer::getIndex(int index) {
-            return &(curvatureScaleArray[index]);
+        else if(!minValid){
+            //cerr << "Min scale is invalid" << endl;
+            return 1;
         }
-
+        else{
+            //cerr << "Max scale is invalid" << endl;
+            return 2;
+        }
     }
     bool UserData::setAnalyisType(int method){
         // Assuming analysisType is an integer representation of the method
@@ -31,32 +53,22 @@ using namespace std;
         return true;
     }
     bool UserData::setHybridSelection(int method1, int method2){
+        // Assuming hybridSelection is an integer representation of the methods
+        // You might need to implement a mapping from string to int
         hybridSelection[0] = method1;
         hybridSelection[1] = method2;
+        return true;
     }
     bool UserData::setInputFilePath(string path){
+        //returns true if file is valid
         inputFilepath = path;
+        return true;
     }
-#include "userInputCLI.h"
-#include "fileHandler.h"
-
-
-bool UserData::setHybridSelection(int obtusemethod, int acutemethod) {
-    // Assuming hybridSelection is an integer representation of the methods
-    // You might need to implement a mapping from string to int
-
-    //returns true if HybridSelection is valid
-}
-
-bool UserData::setInputFilePath(string path) {
-    inputFilepath = path;
-
-    //returns true if file is valid
-
-
-}
 
     //getters
+    DataContainer UserData::getDataContainer(){
+        return dataContainer;
+    }
     int UserData::getMinScale(){
         return userMinScale;
     }
