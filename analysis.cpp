ef6aca3c811e62800b6ac2b0b1c096cb0a8b2bed
LiquidPeanuts <jchang2@wpi.edu>
2025-03-25 23:58:03 -0400

    properly implement multithreading with cmake build directive, implement buffered write

--- analysis.cpp
+++ analysis.cpp
@@ -17,13 +17,15 @@ void analysis::singleAnalysis(UserData* uData, DataContainer* data, double (*met
     double curvature;
     int minScale = uData->getMinScale();
     int maxScale = uData->getMaxScale();
-    #pragma omp parallel for
+
+    // Parallelize the outer loop for better performance
+    #pragma omp parallel for private(curvature) schedule(dynamic)
     for(int scale = minScale; scale <= maxScale; scale++){ //iterate over scales
         for(int point = scale; point < numPoints - scale; point++){ //iterate over points
             // point is the center point 
             // Call the function on the points
             curvature = method( data->getPointAddress(point - scale), data->getPointAddress(point), data->getPointAddress(point + scale)); 
-            // std::cout<<curvature<<std::endl;
+            // Store the result in the data container
             data->putData(scale - minScale, point-scale, curvature); //add curvature to data
         }
     }
